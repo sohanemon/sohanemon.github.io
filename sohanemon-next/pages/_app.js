e3280c21b21671f31a187a7238e4d6ebd96768b7
Sohanur Rahman Emon <m.sohanemon@gmail.com>
2022-07-11 16:07:40 +0600

    verification in head meta

--- sohanemon-next/pages/_app.js
+++ sohanemon-next/pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="google-site-verification" content="lg_txGULhavtXwQDbXW0_2NUBpgwHP3PzTr33CIDaNE" />
+    </Head>
+    <Component {...pageProps} /></>
 }
 
 export default MyApp
