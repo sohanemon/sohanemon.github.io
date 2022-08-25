c3b106071deb1e529b8f6a42288c3dac50d87d12
Sohanur Rahman Emon <m.sohanemon@gmail.com>
2022-08-25 15:23:38 +0600

    Update _app.js

--- sohanemon-next/pages/_app.js
+++ sohanemon-next/pages/_app.js
@@ -4,6 +4,7 @@ function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <meta name="google-site-verification" content="lg_txGULhavtXwQDbXW0_2NUBpgwHP3PzTr33CIDaNE" />
+      <meta name="p:domain_verify" content="3163269bfd98ebd3e07f343b8cccc6f0"/>
     </Head>
     <Component {...pageProps} /></>
 }
